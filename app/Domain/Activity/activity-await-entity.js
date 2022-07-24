eec409ff94659d5c12422907eb66ce19233dbdc4
murilomontino <murilomontinojr@hotmail.com>
2022-07-24 01:07:31 -0300

    [fix] correções de atividade em espera

--- app/Domain/Activity/activity-await-entity.js
+++ app/Domain/Activity/activity-await-entity.js
@@ -32,8 +32,9 @@ class ActivityAwaitEntity {
         color: procedure?.color || null,
         val: procedure?.val || null,
         health_insurance: {
-          value: procedure?.health_insurance?.value || null,
-          label: procedure?.health_insurance?.label || null,
+          value: procedure?.health_insurance?.value || procedure?.health_insurance_id || null,
+          label: procedure?.health_insurance?.label || procedure?.health_insurance_label || null,
+          price: procedure?.health_insurance?.price || procedure?.health_insurance_price || null,
         },
         status: procedure?.status || null,
       })));
