6a28e2585cdeed98431ee10805e7f319b261fe11
murilomontino <murilomontinojr@hotmail.com>
2023-02-08 19:06:30 -0300

    refactor partial unities #42

--- app/Models/Unity.ts
+++ app/Models/Unity.ts
@@ -1,7 +1,20 @@
-import Mongoose, { Schema } from '@ioc:Mongoose';
+import Mongoose, { InferSchemaType, Schema } from '@ioc:Mongoose'
 
-export default Mongoose.model('unities',
-  new Schema()
-)
+const UnitySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+})
 
+export type IUnity = InferSchemaType<typeof UnitySchema>
 
+export default Mongoose.model('unities', UnitySchema)
