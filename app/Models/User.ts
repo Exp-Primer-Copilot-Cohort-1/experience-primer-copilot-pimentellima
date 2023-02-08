f052a533804f36cafb96514a3a20da796377bed2
murilomontino <murilomontinojr@hotmail.com>
2023-02-08 14:19:15 -0300

    refactor partial user #11

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,31 @@
-import Mongoose, { Schema } from '@ioc:Mongoose';
+import Mongoose, { Schema } from '@ioc:Mongoose'
 
 class User extends Schema {
+  constructor() {
+    super({
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      },
+    })
+  }
 }
 
-export default Mongoose.model('User', new User());
+export default Mongoose.model('User', new User())
