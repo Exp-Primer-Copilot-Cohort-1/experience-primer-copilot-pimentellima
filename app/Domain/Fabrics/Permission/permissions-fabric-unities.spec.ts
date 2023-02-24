694a4b9e39c10e512142242f2f14e63052b9ad31
murilomontino <murilomontinojr@hotmail.com>
2023-02-24 09:26:32 -0300

    build(scripts): criado script para testes unitários e de integração com vitest
    closed #45

---
+++ app/Domain/Fabrics/Permission/permissions-fabric-unities.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('should test unitary fabric', () => {
+  it('should true be true (Unitary)', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config'
+
+import { config } from 'dotenv'
+config()
+
+export default defineConfig({
+  root: __dirname,
+  test: {
+    coverage: {
+      provider: 'istanbul', // or 'c8'
+    },
+    reporters: ['html', 'default'],
+    testTimeout: 5000000,
+    deps: {
+      inline: true,
+    },
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.docker/**', '**/tests/e2e/**'],
+  },
+  resolve: {
+    alias: {},
+  },
+})
