595783f81a1ddf7d1115cb1f29c7e7020070d29e
Elvis <nediaulog.gmail.com>
2023-04-10 12:25:06 -0300

    refactor(room, @types): refatoração de room
    refatoração de room e criação da interface para tipagem
    
    re #32

---
+++ app/Models/Room.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from '@ioc:Mongoose';
+import { IRoom } from 'Types/IRoom';
+
+const RoomSchema = new Schema<IRoom>(
+  {
+    active: { type: Boolean, required: true },
+    unity_id: { type: Schema.Types.ObjectId, required: true },
+    support: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+export default model<IRoom>('rooms', RoomSchema);
