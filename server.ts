adb251b37941e84a1975131ce467def007fe9daf
murilomontino <murilomontinojr@hotmail.com>
2023-04-18 14:11:37 -0300

    fix(adonisrc.json): correção no caminho de arquivos de testes

--- server.ts
+++ server.ts
@@ -10,10 +10,10 @@
 |
 */
 
-import 'reflect-metadata'
-import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
+import 'reflect-metadata'
+import { install } from 'source-map-support'
 
-sourceMapSupport.install({ handleUncaughtExceptions: false })
+install({ handleUncaughtExceptions: false })
 
 new Ignitor(__dirname).httpServer().start()
