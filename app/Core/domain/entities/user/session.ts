8c8818eeca4cc56cf2713f169597a6b11ff8829e
murilomontino <murilomontinojr@hotmail.com>
2023-04-21 10:33:40 -0300

    refactor(user -> session): entidade user agora é uma entidade de sessão

--- app/Core/domain/entities/user/session.ts
+++ app/Core/domain/entities/user/session.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import { AbstractError } from 'App/Core/errors/error.interface';
+import { PromiseEither, left, right } from 'App/Core/shared';
 import { IUser } from 'Types/IUser';
+import { UserNotValidError } from '../../errors/user-not-valid';
 import { SystemUser } from '../abstract/system-user.abstract';
+import DaysOfTrade from '../helpers/days-of-trade';
 
-class User extends SystemUser implements IUser {
+class SesssionUser extends SystemUser {
 	public due_date: string;
 	public schedule_obs: string;
 	public show_lack: boolean;
@@ -145,45 +149,40 @@ class User extends SystemUser implements IUser {
 		return {
 			...user,
 			...dayOfTrade.params(),
+			email: this.email,
+			document: this.document,
 		};
 	}
 
-	public static build(user: IUser): User {
-		const {
-			_id,
-			name,
-			email,
-			password,
-			active,
-			avatar,
-			created_at,
-			due_date,
-			schedule_obs,
-			show_lack,
-			type,
-			unity_id,
-			updated_at,
-			rememberMeToken,
-			...dayOfTrade
-		} = user;
+	public static async build(
+		data: IUser,
+	): PromiseEither<AbstractError, SesssionUser> {
+		try {
+			const daysOfTrade = DaysOfTrade.build(data);
+			const user = new SesssionUser()
+				.defineId(data._id)
+				.defineName(data.name)
+				.defineEmail(data.email)
+				.definePassword(data.password)
+				.defineActive(data.active)
+				.defineAvatar(data.avatar)
+				.defineCreatedAt(data.created_at)
+				.defineDueDate(data.due_date)
+				.defineDayOfTrade(daysOfTrade)
+				.defineDocument(data.document)
+				.defineCelphone(data.celphone)
+				.defineScheduleObs(data.schedule_obs)
+				.defineShowLack(data.show_lack)
+				.defineType(data.type)
+				.defineUnityId(data.unity_id.toString())
+				.defineUpdatedAt(data.updated_at)
+				.defineRememberMeToken(data.rememberMeToken);
 
-		return new User()
-			.defineId(_id)
-			.defineName(name)
-			.defineEmail(email)
-			.definePassword(password)
-			.defineActive(active)
-			.defineAvatar(avatar)
-			.defineCreatedAt(created_at)
-			.defineDueDate(due_date)
-			.defineDayOfTrade(dayOfTrade)
-			.defineScheduleObs(schedule_obs)
-			.defineShowLack(show_lack)
-			.defineType(type)
-			.defineUnityId(unity_id.toString())
-			.defineUpdatedAt(updated_at)
-			.defineRememberMeToken(rememberMeToken);
+			return right(user);
+		} catch (error) {
+			return left(new UserNotValidError(error));
+		}
 	}
 }
 
-export default User;
+export default SesssionUser;
