eb2e1babcd7240218560546e38d1a9f4e218eaf6
murilomontino <murilomontinojr@hotmail.com>
2023-04-21 10:41:31 -0300

    refactor(system-user): adicionado getters para dayOfTrade

--- app/Core/domain/entities/user/session.ts
+++ app/Core/domain/entities/user/session.ts
@@ -12,48 +12,6 @@ class SesssionUser extends SystemUser {
 	public show_lack: boolean;
 	public rememberMeToken?: string;
 
-	public get exib_minutes(): number {
-		return this.dayOfTrade.exib_minutes;
-	}
-
-	public get hour_end(): string {
-		return this.dayOfTrade.hour_end;
-	}
-	public get hour_end_lunch(): string {
-		return this.dayOfTrade.hour_end_lunch;
-	}
-	public get hour_start(): string {
-		return this.dayOfTrade.hour_start;
-	}
-	public get hour_start_lunch(): string {
-		return this.dayOfTrade.hour_start_lunch;
-	}
-	public get is_friday(): boolean {
-		return this.dayOfTrade.is_friday;
-	}
-	public get is_monday(): boolean {
-		return this.dayOfTrade.is_monday;
-	}
-	public get is_saturday(): boolean {
-		return this.dayOfTrade.is_saturday;
-	}
-	public get is_sunday(): boolean {
-		return this.dayOfTrade.is_sunday;
-	}
-	public get is_thursday(): boolean {
-		return this.dayOfTrade.is_thursday;
-	}
-	public get is_tuesday(): boolean {
-		return this.dayOfTrade.is_tuesday;
-	}
-	public get is_wednesday(): boolean {
-		return this.dayOfTrade.is_wednesday;
-	}
-
-	public get lunch_time_active(): boolean {
-		return this.dayOfTrade.lunch_time_active;
-	}
-
 	constructor() {
 		super();
 	}
@@ -75,71 +33,6 @@ class SesssionUser extends SystemUser {
 
 	public defineRememberMeToken(rememberMeToken?: string): this {
 		this.rememberMeToken = rememberMeToken;
-		return this;
-	}
-
-	public defineExibMinutes(exib_minutes: number): this {
-		this.dayOfTrade?.defineExibMinutes(exib_minutes);
-		return this;
-	}
-
-	public defineHourEnd(hour_end: string): this {
-		this.dayOfTrade?.defineHourEnd(hour_end);
-		return this;
-	}
-
-	public defineHourEndLunch(hour_end_lunch: string): this {
-		this.dayOfTrade?.defineHourEndLunch(hour_end_lunch);
-		return this;
-	}
-
-	public defineHourStart(hour_start: string): this {
-		this.dayOfTrade?.defineHourStart(hour_start);
-		return this;
-	}
-
-	public defineHourStartLunch(hour_start_lunch: string): this {
-		this.dayOfTrade?.defineHourStartLunch(hour_start_lunch);
-		return this;
-	}
-
-	public defineIsFriday(is_friday: boolean): this {
-		this.dayOfTrade?.defineIsFriday(is_friday);
-		return this;
-	}
-
-	public defineIsMonday(is_monday: boolean): this {
-		this.dayOfTrade?.defineIsMonday(is_monday);
-		return this;
-	}
-
-	public defineIsSaturday(is_saturday: boolean): this {
-		this.dayOfTrade?.defineIsSaturday(is_saturday);
-		return this;
-	}
-
-	public defineIsSunday(is_sunday: boolean): this {
-		this.dayOfTrade?.defineIsSunday(is_sunday);
-		return this;
-	}
-
-	public defineIsThursday(is_thursday: boolean): this {
-		this.dayOfTrade?.defineIsThursday(is_thursday);
-		return this;
-	}
-
-	public defineIsTuesday(is_tuesday: boolean): this {
-		this.dayOfTrade?.defineIsTuesday(is_tuesday);
-		return this;
-	}
-
-	public defineIsWednesday(is_wednesday: boolean): this {
-		this.dayOfTrade?.defineIsWednesday(is_wednesday);
-		return this;
-	}
-
-	public defineLunchTimeActive(lunch_time_active: boolean): this {
-		this.dayOfTrade?.defineLunchTimeActive(lunch_time_active);
 		return this;
 	}
 
