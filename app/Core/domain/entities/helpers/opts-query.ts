98a6c1f3550e1a7d75ec6b24302ac1d8d252df23
murilomontino <murilomontinojr@hotmail.com>
2023-05-04 12:04:40 -0300

    chore(vite): exclude testes units para composers e adapters

--- app/Core/domain/entities/helpers/opts-query.ts
+++ app/Core/domain/entities/helpers/opts-query.ts
@@ -1,51 +1,51 @@
-import { IOptsQuery } from 'Types/IOptsQuery';
+import { IOptsQuery } from 'Types/IOptsQuery'
 
 export class OptsQuery implements IOptsQuery {
-	private _sort: string;
-	private _limit: number;
-	private _skip: number;
-	private _active: boolean;
+	private _sort: string
+	private _limit: number
+	private _skip: number
+	private _active: boolean
 
 	private constructor() { }
 
 	public get sort(): string {
-		return this._sort;
+		return this._sort
 	}
 
 	public get limit(): number {
-		return this._limit;
+		return this._limit
 	}
 
 	public get skip(): number {
-		return this._skip;
+		return this._skip
 	}
 
 	public get active(): boolean {
-		return this._active;
+		return this._active
 	}
 
-	defineSort(sort: string = '--name'): OptsQuery {
-		this._sort = sort;
-		return this;
+	defineSort(sort = '--name'): OptsQuery {
+		this._sort = sort
+		return this
 	}
 
-	defineSize(limit: number = 100): OptsQuery {
-		this._limit = Number(limit);
-		return this;
+	defineSize(limit = 100): OptsQuery {
+		this._limit = Number(limit)
+		return this
 	}
 
-	defineSkip(skip: number = 0): OptsQuery {
-		this._skip = Number(skip);
-		return this;
+	defineSkip(skip = 0): OptsQuery {
+		this._skip = Number(skip)
+		return this
 	}
 
-	defineActive(active: boolean = true): OptsQuery {
+	defineActive(active = true): OptsQuery {
 		if (typeof active === 'string') {
-			active = active === 'true';
+			active = active === 'true'
 		}
 
-		this._active = Boolean(active);
-		return this;
+		this._active = Boolean(active)
+		return this
 	}
 
 	static build(opts?: any): OptsQuery {
@@ -53,6 +53,6 @@ export class OptsQuery implements IOptsQuery {
 			.defineSort(opts?.sort)
 			.defineSize(opts?.limit)
 			.defineSkip(opts?.skip)
-			.defineActive(opts?.active);
+			.defineActive(opts?.active)
 	}
 }
