8615f8e18581e551fe6601852287457babd43c61
murilomontino <murilomontinojr@hotmail.com>
2023-05-15 10:18:17 -0300

    feat(sendsignupconfirmemail): decorador de email de confirmação de conta criado

---
+++ app/Mail/emails/send-sign-up-confirm-email.ts
@@ -0,0 +1,45 @@
+// Decorador de função
+import Env from '@ioc:Adonis/Core/Env'
+import { Either } from 'App/Core/shared'
+import EDGE from '../constants/edge'
+import Mail from '../entity/mail'
+
+export default function SendEmailSignUpConfirm(
+	target: any,
+	propertyKey: string,
+	descriptor: PropertyDescriptor,
+): PropertyDescriptor {
+	const originalMethod = descriptor.value
+
+	descriptor.value = async function (...args: any[]) {
+		const result: Either<any, any> = await originalMethod.apply(this, args)
+
+		if (result.isLeft()) {
+			return result
+		}
+
+		const user = result.extract()
+
+		await Promise.all([
+			Mail.send({
+				edge: EDGE.confirm_user,
+				props: {
+					site_activation: `${Env.get('URL')}/account-activation/${user.id}`,
+					label: user.label || user.name,
+				},
+				email: user.email,
+				title: 'Ative sua conta na DPSystem',
+			}),
+			Mail.send({
+				edge: EDGE.new_account,
+				props: { user_email: user.email },
+				title: 'Um novo cadastro',
+				email: Env.get('SMTP_USERNAME'),
+			}),
+		])
+
+		return result
+	}
+
+	return descriptor
+}
