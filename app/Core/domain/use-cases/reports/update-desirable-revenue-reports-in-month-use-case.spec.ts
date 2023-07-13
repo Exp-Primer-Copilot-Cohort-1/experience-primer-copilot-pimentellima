c28b3f90a8582f28146cd5ca0df3657d092a98f1
murilomontino <murilomontinojr@hotmail.com>
2023-07-13 14:58:18 -0300

    feat: agora o valor desejado é configurado em todos os meses seguintes

---
+++ app/Core/domain/use-cases/reports/update-desirable-revenue-reports-in-month-use-case.spec.ts
@@ -0,0 +1,32 @@
+import { BillingMongooseRepository } from 'App/Core/domain/repositories'
+import mongoose from 'mongoose'
+import { beforeAll, describe, expect, it } from 'vitest'
+
+import { UpdateDesirableBillingInMonthUseCase } from './update-desirable-revenue-reports-in-month-use-case'
+
+const makeSut = () => {
+	const sut = new UpdateDesirableBillingInMonthUseCase(new BillingMongooseRepository())
+	return {
+		sut,
+	}
+}
+
+describe('Update Desirable Revenue Reports in Month Use Case (Integration)', () => {
+	beforeAll(async () => {
+		await mongoose.connect(
+			'mongodb://admin:admin@localhost/admin?connectTimeoutMS=300000&retryWrites=true',
+		)
+	})
+
+	it('should be able to update billing reports in month', async () => {
+		const { sut } = makeSut()
+
+		const revenueOrErr = await sut.execute({
+			unity_id: '6359660fc109b232759921d4',
+			value: 15,
+			month: 1,
+		})
+
+		expect(revenueOrErr.isRight()).toBeTruthy()
+	})
+})
---
+++ app/Core/domain/use-cases/reports/update-desirable-revenue-reports-in-month-use-case.ts
@@ -0,0 +1,35 @@
+import { AbstractError } from 'App/Core/errors/error.interface'
+import { UseCase } from 'App/Core/interfaces/use-case.interface'
+import { PromiseEither, left, right } from 'App/Core/shared'
+import { IBilling } from 'Types/IBilling'
+import { ReportsUnitiesManagerInterface } from '../../repositories/interface/reports-unities-manager.interface'
+import { UpdateAttrReportsRevenues } from '../helpers/reports'
+
+export class UpdateDesirableBillingInMonthUseCase
+	implements UseCase<UpdateAttrReportsRevenues, IBilling>
+{
+	constructor(private readonly manager: ReportsUnitiesManagerInterface) { }
+
+	public async execute({
+		value,
+		unity_id,
+		month = new Date().getMonth(),
+	}: UpdateAttrReportsRevenues): PromiseEither<AbstractError, IBilling> {
+		let billingOrErr
+
+		for (let i = month; i < 12; i++) {
+			billingOrErr = await this.manager.updateAttrBilling(
+				unity_id,
+				'desirable',
+				parseFloat(value.toString()),
+				i,
+			)
+		}
+
+		if (billingOrErr?.isLeft()) {
+			return left(billingOrErr.extract())
+		}
+
+		return right(billingOrErr.extract())
+	}
+}
