757c7c658b359f502add19b5f89c588bb7e6a896
murilomontino <murilomontinojr@hotmail.com>
2023-07-17 19:31:35 -0300

    feat: tipagem e Model de IPayment

--- @types/IPaymentActivity.ts
+++ @types/IPaymentActivity.ts
@@ -1,22 +1,24 @@
-import { ObjectId } from "@ioc:Mongoose";
+import { ObjectId } from '@ioc:Mongoose'
 
 export interface IPaymentActivity {
 	bank: {
-		id: string | ObjectId;
-		name: string;
-	};
+		id: string | ObjectId
+		name: string
+	}
 	cost_center: {
-		id: string | ObjectId;
-		name: string;
-	};
+		id: string | ObjectId
+		name: string
+	}
 	category: {
-		id: string | ObjectId;
-		name: string;
-	};
-	value: string;
-	date: string;
-	paymentForm: string;
-	description?: string;
-	installment: boolean;
-	installments?: number;
+		id: string | ObjectId
+		name: string
+	}
+	value: string
+	date: string
+	paymentForm: string
+	description?: string
+	type?: 'income' | 'expense'
+	installment: boolean
+	installmentCurrent?: number
+	installments?: number
 }
--- app/Models/PaymentActivity.ts
+++ app/Models/PaymentActivity.ts
@@ -1,7 +1,72 @@
 import Mongoose, { Schema } from '@ioc:Mongoose'
+import { IPaymentActivity } from 'Types/IPaymentActivity'
 
-const PaymentActivitySchema = new Schema(
-	{},
+const schemaDefault = (colection: string) => ({
+	type: Schema.Types.ObjectId,
+	ref: colection,
+	required: false,
+})
+
+const PaymentActivitySchema = new Schema<IPaymentActivity>(
+	{
+		bank: {
+			id: schemaDefault('banks'),
+			name: {
+				type: String,
+				required: false,
+			},
+		},
+		cost_center: {
+			id: schemaDefault('cost_centers'),
+			name: {
+				type: String,
+				required: false,
+			},
+		},
+		category: {
+			id: schemaDefault('financial_categories'),
+			name: {
+				type: String,
+				required: false,
+			},
+		},
+		value: {
+			type: String,
+			required: true,
+		},
+		paymentForm: {
+			type: String,
+			required: true,
+		},
+		date: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: false,
+		},
+		type: {
+			type: String,
+			required: true,
+			enum: ['income', 'expense'],
+		},
+		installment: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+		installmentCurrent: {
+			type: Number,
+			required: false,
+			default: 1,
+		},
+		installments: {
+			type: Number,
+			required: false,
+			default: 1,
+		},
+	},
 	{
 		timestamps: {
 			createdAt: 'created_at',
