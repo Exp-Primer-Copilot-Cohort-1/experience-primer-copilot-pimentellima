2f05085d3a77e92313c8cba4449e5068652f78e6
pimentellima <matheus_dere@hotmail.com>
2023-07-18 11:16:06 -0300

    feat: mudança na rota de verificar os horários para retornar os horários do profissional

--- app/Controllers/Http/ScheduleController.ts
+++ app/Controllers/Http/ScheduleController.ts
@@ -1,10 +1,10 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { adaptRoute } from "App/Core/adapters";
-import { makeVerifyScheduleComposer } from "App/Core/composers/schedule/schedule-composer";
+import { makeGetWorkHoursComposer } from "App/Core/composers/schedule/schedule-composer";
 
 class ScheduleController {
-	async verifySchedule(ctx: HttpContextContract) {
-		return adaptRoute(makeVerifyScheduleComposer(), ctx);
+	async getWorkHours(ctx: HttpContextContract) {
+		return adaptRoute(makeGetWorkHoursComposer(), ctx);
 	}
 }
 
--- app/Core/domain/repositories/interface/schedule-manager-interface.ts
+++ app/Core/domain/repositories/interface/schedule-manager-interface.ts
@@ -1,23 +1,14 @@
 import { AbstractError } from "App/Core/errors/error.interface";
 import { PromiseEither } from "App/Core/shared";
-import { ScheduleParams } from "Types/ISchedule";
-import * as z from "zod";
 
 export interface ScheduleManagerInterface {
-	verifySchedule(
-		prof_id: string,
-		schedule: ScheduleParams
-	): PromiseEither<
+	getWorkHours(prof_id: string): PromiseEither<
 		AbstractError,
-		z.SafeParseReturnType<
-			{
-				hourEnd: string;
-				hourStart: string;
-			},
-			{
-				hourEnd: string;
-				hourStart: string;
-			}
-		>
+		{
+			hourStartDay: string;
+			hourEndDay: string;
+			hourStartLunch: string;
+			hourEndLunch: string;
+		}
 	>;
 }
