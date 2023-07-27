05be05b67c9fb3633b9ba26682c90c2c8eafba30
murilomontino <murilomontinojr@hotmail.com>
2023-07-27 13:12:33 -0300

    refactor: mudança na lógica de scores

--- app/Core/domain/repositories/helpers/scores.ts
+++ app/Core/domain/repositories/helpers/scores.ts
@@ -6,35 +6,59 @@ type Scores = {
 }
 
 export async function generateScores(unity_id: ObjectId): Promise<Scores> {
-	const scores = await Activity.aggregate([
+	const scores: Scores[] = await Activity.aggregate([
 		{
 			$match: {
 				type: 'marked',
 				unity_id: unity_id,
 				date: { $lte: new Date() },
+				scheduled: { $in: ['completed', 'canceled_client', 'canceled'] },
 			},
 		},
 		{
 			$group: {
 				_id: '$client.value',
 				total: { $sum: 1 },
-				completed: {
-					$sum: { $cond: [{ $eq: ['$scheduled', 'completed'] }, 1, 0] },
+				canceled: {
+					$sum: {
+						$cond: [
+							{
+								$eq: ['$scheduled', ['canceled_client', 'canceled']],
+							},
+							1,
+							0,
+						],
+					},
 				},
 			},
 		},
 		{
 			$project: {
 				_id: 1,
-				score: { $divide: ['$completed', '$total'] },
+				score: {
+					$round: [
+						{
+							$subtract: [
+								100,
+								{
+									$multiply: [
+										{ $divide: ['$canceled', '$total'] },
+										100,
+									],
+								},
+							],
+						},
+						2,
+					],
+				},
 			},
 		},
 	])
 
 	// Converte a pontuação em um objeto para facilitar a pesquisa
 	const scoreMap: Scores = {}
-	for (const score of scores) {
-		scoreMap[score._id] = ((score.score as number) * 100).toFixed(2)
+	for (const item of scores) {
+		scoreMap[item._id] = item.score
 	}
 
 	return scoreMap
