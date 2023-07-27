202c334903c69f24490ea1f009fa9a992dbe9978
pimentellima <matheus_dere@hotmail.com>
2023-07-27 11:27:01 -0300

    feat: modificar estoque dos produtos dos procedimentos ao concluir a atividade

--- app/Core/domain/use-cases/activities/update-activity-status-by-use-case.ts
+++ app/Core/domain/use-cases/activities/update-activity-status-by-use-case.ts
@@ -3,7 +3,11 @@ import { AbstractError } from "App/Core/errors/error.interface";
 import { UseCase } from "App/Core/interfaces/use-case.interface";
 import { PromiseEither, left, right } from "App/Core/shared";
 import { AppointmentStatus } from "App/Helpers";
+import Activity from "App/Models/Activity";
+import Procedure from "App/Models/Procedure";
+import Stock from "App/Models/Stock";
 import { IActivity } from "Types/IActivity";
+import { ActivityNotFoundError } from "../../errors/activity-not-found";
 
 type Props = {
 	id: string;
@@ -28,6 +32,55 @@ export class UpdateActivityStatusByIdUseCase
 
 		if (updatedActivityOrErr.isLeft())
 			return left(updatedActivityOrErr.extract());
+
+		if (
+			updatedActivityOrErr.extract().scheduled ===
+			AppointmentStatus.COMPLETED
+		) {
+			const activity = await Activity.findById(params.id);
+			if (!activity) return left(new ActivityNotFoundError());
+			const stocksData = await Stock.find();
+
+			const proceduresData = await Procedure.find({
+				_id: {
+					$in: activity.procedures.map((p) => p.value),
+				},
+			});
+			const productsWithQuantities = new Object();
+			activity.procedures.forEach((procedure) => {
+				const products = proceduresData.find(
+					(p) => p._id.toString() === procedure.value
+				)?.products;
+
+				if (!products) return;
+				products.forEach((product) => {
+					const productData = stocksData.find(
+						(p) => p._id.toString() === product.value
+					);
+					if (!productData?.stock_automatic) return;
+					if (productsWithQuantities[product.value]) {
+						productsWithQuantities[product.value] +=
+							product.quantity;
+					} else
+						productsWithQuantities[product.value] =
+							product.quantity;
+				});
+			});
+
+			Object.keys(productsWithQuantities).forEach(async (key) => {
+				await Stock.findByIdAndUpdate(
+					{ _id: key },
+					{
+						$inc: { quantity: -productsWithQuantities[key] },
+					},
+					{
+						new: true,
+						returnDocument: "after",
+					}
+				);
+			});
+		}
+
 		return right(updatedActivityOrErr.extract());
 	}
 }
