79e1b1f58a3ad8f1c944b76953eb6ab8b7e506c2
pimentellima <matheus_dere@hotmail.com>
2023-08-01 11:26:51 -0300

    refactor: mudança no modelo de stock para aceitar lote único

--- app/Models/Stock.ts
+++ app/Models/Stock.ts
@@ -6,14 +6,15 @@ const StockSchema = new Schema<IStock>(
 		name: { type: String, required: true },
 		batches: [
 			{
-				name: { type: String, required: true },
+				name: { type: String, required: false },
 				quantity: { type: Number, required: true },
 				minimum_quantity: { type: Number, required: true },
-				date_batch: { type: Date, required: true },
+				date_batch: { type: Date, required: false },
 				price_cost: { type: String, required: true },
 				price_final: { type: String, required: true },
 			},
 		],
+		single_lot: { type: Boolean, required: true },
 		stock_automatic: { type: Boolean, required: true },
 		active: { type: Boolean, required: true },
 		unity_id: { type: Schema.Types.ObjectId, required: true },
