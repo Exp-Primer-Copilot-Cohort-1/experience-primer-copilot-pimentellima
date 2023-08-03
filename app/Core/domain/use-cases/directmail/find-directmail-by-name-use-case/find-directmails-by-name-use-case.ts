283a55661345e07c8d6dd909ff553df2a802e9ff
pimentellima <matheus_dere@hotmail.com>
2023-08-03 10:25:00 -0300

    fix: correção de aceitar busca sem parametro de nome na rota de buscar atestados

--- app/Core/domain/use-cases/directmail/find-directmail-by-name-use-case/find-directmails-by-name-use-case.ts
+++ app/Core/domain/use-cases/directmail/find-directmail-by-name-use-case/find-directmails-by-name-use-case.ts
@@ -1,7 +1,6 @@
 import { AbstractError } from 'App/Core/errors/error.interface'
 import { UseCase } from 'App/Core/interfaces/use-case.interface'
 import { PromiseEither, left, right } from 'App/Core/shared'
-import { MissingParamsError } from '../../../errors/missing-params'
 import { DirectmailManagerInterface } from '../../../repositories/interface'
 
 type FindAllProps = {
@@ -13,11 +12,9 @@ export class FindDirectmailsByNameUseCase implements UseCase<FindAllProps, any[]
 	constructor(private readonly directmailManager: DirectmailManagerInterface) { }
 
 	public async execute(input: FindAllProps): PromiseEither<AbstractError, any[]> {
-		if (!input?.name) {
-			return left(new MissingParamsError('name'))
-		}
+		
 		const directmailOrErr = await this.directmailManager.findByName(
-			input.name,
+			input.name || '',
 			input.unity_id,
 		)
 		if (directmailOrErr.isLeft()) {
