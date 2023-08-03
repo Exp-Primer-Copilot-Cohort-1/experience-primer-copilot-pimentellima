5e7fe38b68dc0212c2d73a9fe94664ea35c10817
murilomontino <murilomontinojr@hotmail.com>
2023-08-03 16:56:54 -0300

    ci: criado econssystem para pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config()
+
+module.exports = {
+	apps: [
+		{
+			name: 'dpsystem',
+			script: 'yarn',
+			args: 'start',
+			watch: true,
+			max_memory_restart: '150M',
+			autorestart: true,
+
+			// instances: 'max',
+			// exec_mode: 'cluster',
+		},
+	],
+}
