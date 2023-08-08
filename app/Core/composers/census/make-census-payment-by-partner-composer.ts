07b59618ed476886bcd9fc721ffdc46f69f8173c
Elvis <nediaulog.gmail.com>
2023-08-08 17:40:36 -0300

    refactor: separação do caso de uso findpaymentsbypartner e composer

---
+++ app/Core/composers/census/make-census-payment-by-partner-composer.ts
@@ -0,0 +1,10 @@
+import { Controller } from 'App/Core/adapters/controller'
+import { ControllerGeneric } from 'App/Core/adapters/controller/helpers'
+import { CensusPaymentsMongooseRepository } from 'App/Core/domain/repositories'
+import { FindPaymentsByPartnersUseCase } from 'App/Core/domain/use-cases'
+
+export const makeCensusPaymentsByPartnerComposer = (): ControllerGeneric => {
+	return new Controller(
+		new FindPaymentsByPartnersUseCase(new CensusPaymentsMongooseRepository()),
+	)
+}
