ed009cd480aa661ba5e5a07ab5cf4e8f2c6d0b21
murilomontino <murilomontinojr@hotmail.com>
2023-09-12 10:59:05 -0300

    refactor: controller englobado num try cath para melhor leitura

--- app/Core/adapters/controller/ports/controller.ts
+++ app/Core/adapters/controller/ports/controller.ts
@@ -2,36 +2,29 @@
 import { HttpRequest } from 'App/Core/adapters/controller/ports/http'
 import { err, ok } from 'App/Core/adapters/helpers/http-helper'
 import { UseCase } from 'App/Core/interfaces/use-case.interface'
-import promiseErrorHandler from '../helpers/promise-err-handler'
 
 import { AbstractError } from 'App/Core/errors/error.interface'
 import { IUser } from 'Types/IUser'
 import { ControllerGeneric } from '../helpers/controller-generic'
 
 export class Controller implements ControllerGeneric {
-	constructor(private readonly UseCase: UseCase<any, any>) { }
+	constructor(private readonly UseCase: UseCase<any, unknown>) { } // eslint-disable-line
 
 	public async handle(httpRequest: HttpRequest, user?: IUser) {
-		const [error, resOrErr] = await promiseErrorHandler(
-			this.UseCase.execute(httpRequest.body, user),
-		)
+		try {
+			const useCaseOrErr = await this.UseCase.execute(httpRequest.body, user)
 
-		if (error) {
-			if (process.env.NODE_ENV !== 'production') {
-				console.log('Controller: ', error)
+			if (useCaseOrErr.isLeft()) {
+				if (process.env.NODE_ENV !== 'production') {
+					console.log('Controller: ', useCaseOrErr.extract())
+				}
+				return err(useCaseOrErr.extract())
 			}
+
+			return ok(useCaseOrErr.extract())
+		} catch (error) {
 			return err(new AbstractError(error.message, 500, error))
 		}
-
-		if (resOrErr.isLeft()) {
-			if (process.env.NODE_ENV !== 'production') {
-				console.log(resOrErr.extract())
-			}
-
-			return err(resOrErr.extract())
-		}
-
-		return ok(resOrErr.extract())
 	}
 }
 
