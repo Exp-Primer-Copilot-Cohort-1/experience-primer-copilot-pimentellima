fe6c35e3fd7917a93678ed2d8530ffa8f0be01a3
pimentellima <matheus_dere@hotmail.com>
2023-09-15 14:40:56 -0300

    fix: bug atividade não finalizava

--- app/Core/domain/use-cases/activities/update-activity-finished-at-use-case.ts
+++ app/Core/domain/use-cases/activities/update-activity-finished-at-use-case.ts
@@ -1,5 +1,5 @@
 import LogDecorator, { ACTION } from 'App/Core/decorators/log-decorator'
-import { ActivitiesManagerAttendanceInterface } from 'App/Core/domain/repositories/interface'
+import { ActivitiesManagerAttendanceInterface, ActivitiesManagerInterface } from 'App/Core/domain/repositories/interface'
 import { AbstractError } from 'App/Core/errors/error.interface'
 import { UseCase } from 'App/Core/interfaces/use-case.interface'
 import { PromiseEither, left, right } from 'App/Core/shared'
@@ -17,30 +17,31 @@ type Props = {
 
 export class UpdateActivityFinishedAtUseCase implements UseCase<Props, IActivity> {
 	constructor(
-		private readonly activitiesManager: ActivitiesManagerAttendanceInterface,
-	) { } // eslint-disable-line
+		private readonly activityAttendanceManager: ActivitiesManagerAttendanceInterface,
+		private readonly activityManager: ActivitiesManagerInterface
+	) {} // eslint-disable-line
 
 	@LogDecorator(COLLECTION_NAME, ACTION.PUT)
 	public async execute(params: Props): PromiseEither<AbstractError, IActivity> {
-		const activity = await Activity.findById(params.id)
+		/* const activity = await this.activityManager.findActivityById(params.id)
 		if (!activity) return left(new ActivityNotFoundError())
-
-		if (!activity) return left(new AbstractError('', 500))
+		if (activity.isLeft()) return left(activity.extract())
+		const extractedActivity = activity.extract()
 		const stocksData = await Stock.find()
 		const proceduresData = await Procedure.find({
 			_id: {
-				$in: activity.procedures.map((p) => p.info.value),
+				$in: extractedActivity.procedures.map((p) => p.info.value),
 			},
 		})
 		const productsWithQuantities = new Object()
-		activity.procedures.forEach((procedure) => {
+		extractedActivity.procedures.forEach((procedure) => {
 			const products = proceduresData.find(
 				(p) => p._id.toString() === procedure.info.value,
 			)?.products
 
 			if (!products) return
 			products.forEach((product) => {
-				const productData = stocksData.find(
+				const productData = stocksData?.find(
 					(stock) => stock._id.toString() === product.value.toString(),
 				)
 
@@ -55,7 +56,7 @@ export class UpdateActivityFinishedAtUseCase implements UseCase<Props, IActivity
 		await Promise.all(
 			Object.keys(productsWithQuantities).map(async (key) => {
 				const stock: IStock | undefined = stocksData
-					.find((stock) => stock._id.toString() === key)
+					?.find((stock) => stock._id.toString() === key)
 					?.toObject()
 				if (!stock?.batches.length) return
 				let quantityLeft = productsWithQuantities[key]
@@ -85,10 +86,10 @@ export class UpdateActivityFinishedAtUseCase implements UseCase<Props, IActivity
 					},
 				)
 			}),
-		)
+		) */
 
 		const updatedActivityOrErr =
-			await this.activitiesManager.updateActivityFinishedAt(
+			await this.activityAttendanceManager.updateActivityFinishedAt(
 				params.id,
 				params.finished_at,
 			)
