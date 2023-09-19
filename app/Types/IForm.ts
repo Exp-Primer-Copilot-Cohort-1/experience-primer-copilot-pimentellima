e72cac07a09089a154aa0978e1675917f368dc9e
pimentellima <matheus_dere@hotmail.com>
2023-09-19 16:16:01 -0300

    refactor: alteração no modelo do Form para diferenciar prof proprietario de profs com acesso

--- app/Types/IForm.ts
+++ app/Types/IForm.ts
@@ -1,12 +1,15 @@
-import { ObjectId } from "@ioc:Mongoose"
+import { ObjectId } from '@ioc:Mongoose'
+
+export type Prof = {
+	label: string
+	value: string
+}
 
 export interface IForm {
-    _id: ObjectId | string
-	prof: {
-		label: string
-		value: string
-	}[]
-    unity_id: ObjectId | string
+	_id: ObjectId | string
+	prof: Prof
+	profs_with_access: Prof[]
+	unity_id: ObjectId | string
 	name: string
 	fields: any[]
 	active: boolean
