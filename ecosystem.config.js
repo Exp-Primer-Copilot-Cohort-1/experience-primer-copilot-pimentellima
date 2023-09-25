f316b85fef1cd1e77a18f7babe1f97ff06dec88d
murilomontino <murilomontinojr@hotmail.com>
2023-09-25 18:13:51 -0300

    ci: intervalo para pm2 restart o servidor para 5 min após observar alterações

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,16 @@ module.exports = {
 			name: 'dpsystem-v5',
 			script: 'yarn',
 			args: 'start',
-			watch: false,
+			watch: true,
 			max_memory_restart: '200M',
 			autorestart: true,
+			ignore_watch: ['node_modules', 'logs', 'docs', 'tests'], // Adicione os diretórios/arquivos que você deseja ignorar
 			instances: 'max',
 			exec_mode: 'cluster',
+			watch_options: {
+				usePolling: true, // Use polling para detecção de alterações em sistemas de arquivos montados em rede ou em máquinas virtuais
+				interval: 5 * 60 * 1000, // Intervalo de 3 minutos em milissegundos antes de reiniciar após uma alteração			},
+			},
 		},
 	],
 }
