ffae80a6b0dabbe32b3bca298bd00e063efac668
murilomontino <murilomontinojr@hotmail.com>
2023-10-05 16:54:31 -0300

    fix: correção na importação de use cases de partners

--- app/Core/composers/partner/make-partner-update-by-id-composer.ts
+++ app/Core/composers/partner/make-partner-update-by-id-composer.ts
@@ -1,7 +1,7 @@
 import { Controller } from 'App/Core/adapters/controller'
 import { ControllerGeneric } from 'App/Core/adapters/controller/helpers'
 import { PartnerMongooseRepository } from 'App/Core/domain/repositories'
-import { UpdatePartnersByIdUseCase } from 'App/Core/domain/use-cases/partner/partner-update-by-id-use-case/partner-update-by-id-use-case'
+import { UpdatePartnersByIdUseCase } from 'App/Core/domain/use-cases/partner'
 
 export const makePartnerUpdateComposer = (): ControllerGeneric => {
 	return new Controller(new UpdatePartnersByIdUseCase(new PartnerMongooseRepository()))
