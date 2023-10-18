7690f233230f2a8e0ded6416daf725f474c1ee61
murilomontino <murilomontinojr@hotmail.com>
2023-10-18 13:58:31 -0300

    refactor: email de aviso de respostas refatorado para casos de uso

--- app/Listeners/ReplyStandardForm.ts
+++ app/Listeners/ReplyStandardForm.ts
@@ -1,11 +1,9 @@
 import type { EventsList } from '@ioc:Adonis/Core/Event';
-import { RFormSFMongooseManager } from 'App/Core/domain/repositories/reply-form-standard-franchise/reply-form-standard-franchise-mongoose-repository';
-import { VerifyCurrentReplyInLessThanPreviousUseCase } from 'App/Core/domain/use-cases';
-import logger from 'App/Core/infra/logger';
+import {
+	SendCurrentReplyInGreaterPreviousUseCase,
+	VerifyCurrentReplyInLessThanPreviousUseCase
+} from 'App/Core/domain/use-cases';
 import container from 'App/Core/shared/container';
-import EDGE from 'App/Mail/constants/edge';
-import Mail from 'App/Mail/entity/mail';
-import { retry } from 'ts-retry-promise';
 export default class ReplySFormFranchise {
 	async onNewReplyFormStandardFranchise(data: EventsList['new:reply-form-standard-franchise']) {
 		const controller = container.resolve(VerifyCurrentReplyInLessThanPreviousUseCase)
@@ -13,43 +11,7 @@ export default class ReplySFormFranchise {
 	}
 
 	async onNewEmailCurrentReplyInGreaterPrevious(data: EventsList['new:email-current-reply-in-greater-previous']) {
-		const manager = new RFormSFMongooseManager()
-		const infoOrErr = await manager.findInfoThisReply(data.group_id)
-
-		if (infoOrErr.isLeft()) return
-
-		const info = infoOrErr.extract()
-
-		const promiseSendCoordinator = retry(async () => Mail.send({
-			edge: EDGE.reply_current_greater_previous,
-			props: {
-				label: info.coordinator.name,
-				...info
-			},
-			email: info.coordinator.email || info.unity.email, // email do coordenador
-			title: 'Aviso! Paciente não apresentou melhora no tratamento',
-		}), {
-			logger: (msg) => logger.emit(msg),
-			retries: 5
-		})
-
-		const promiseSendProf = retry(async () => Mail.send({
-			edge: EDGE.reply_current_greater_previous,
-			props: {
-				label: info.prof.name,
-				...info
-			},
-			email: info.prof.email, // email do profissional
-			title: 'Aviso! Paciente não apresentou melhora no tratamento',
-		}), {
-			logger: (msg) => logger.emit(msg),
-			retries: 5
-		})
-
-
-		await Promise.all([
-			promiseSendCoordinator,
-			promiseSendProf
-		])
+		const controller = container.resolve(SendCurrentReplyInGreaterPreviousUseCase)
+		await controller.execute(data)
 	}
 }
