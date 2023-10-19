797e7be7d50b4f2ce983c6f272d5cbbdbd3c4692
Elvis <nediaulog.gmail.com>
2023-10-19 00:30:15 -0300

    refactor: refatoracao de medical certificate

---
+++ app/Core/domain/use-cases/medical-certificate/update-medical-certificate-by-id-use-case.ts
@@ -0,0 +1,39 @@
+import { AbstractError } from 'App/Core/errors/error.interface'
+import { UseCase } from 'App/Core/interfaces/use-case.interface'
+import { PromiseEither, left } from 'App/Core/shared'
+
+import { MissingParamsError } from 'App/Core/domain/errors/missing-params'
+import { MedicalCertificateManagerInterface } from 'App/Core/domain/repositories/interface'
+import { IMedicalCertificate } from 'App/Types/IMedicalCertificate'
+import { inject, injectable, registry } from 'tsyringe'
+import { MedicalCertificateMongooseRepository } from '../../repositories'
+
+
+@injectable()
+@registry([{ token: UpdateMedicalCertificateByIdUseCase, useClass: UpdateMedicalCertificateByIdUseCase }])
+export class UpdateMedicalCertificateByIdUseCase
+	implements UseCase<Partial<IMedicalCertificate>, IMedicalCertificate>
+{
+	constructor(
+		@inject(MedicalCertificateMongooseRepository) private readonly manager: MedicalCertificateManagerInterface
+	) { } // eslint-disable-line
+
+	public async execute(
+		medicalCertificate: Partial<IMedicalCertificate>,
+	): PromiseEither<AbstractError, IMedicalCertificate> {
+		if (!medicalCertificate?._id) {
+			return left(new MissingParamsError('_id is required'))
+		}
+		const medicalCertificateOrErr =
+			await this.manager.update(
+				medicalCertificate._id.toString(),
+				medicalCertificate,
+			)
+
+		if (medicalCertificateOrErr.isLeft()) {
+			return left(medicalCertificateOrErr.extract())
+		}
+
+		return medicalCertificateOrErr
+	}
+}
