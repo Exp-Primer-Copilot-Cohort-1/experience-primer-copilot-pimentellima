1c5b6a02de974df53dac79b53242bfb3de86a613
Elvis <nediaulog.gmail.com>
2023-10-27 05:06:12 -0300

    refactor: refatoracao de cost center

---
+++ app/Core/domain/use-cases/cost-center/create-cost-center-use-case.ts
@@ -0,0 +1,29 @@
+import { AbstractError } from 'App/Core/errors/error.interface'
+import { UseCase } from 'App/Core/interfaces/use-case.interface'
+import { PromiseEither } from 'App/Core/shared'
+
+import { ICostCenter } from 'App/Types/ICostCenter'
+import { inject, injectable, registry } from 'tsyringe'
+import { CostCenterManagerContract } from '../../repositories/cost-center/const-center-manager.interface'
+import { CostCenterMongooseRepository } from '../../repositories/cost-center/cost-center-mongo-repository'
+
+@injectable()
+@registry([{ token: CreateCostCenterUseCase, useClass: CreateCostCenterUseCase }])
+export class CreateCostCenterUseCase
+    implements UseCase<Partial<ICostCenter>, ICostCenter>
+{
+    constructor(
+        @inject(CostCenterMongooseRepository) private readonly manager: CostCenterManagerContract
+    ) { }
+
+    public async execute(
+        costCenter: Partial<ICostCenter>,
+    ): PromiseEither<AbstractError, ICostCenter> {
+        const costCenterOrErr =
+            await this.manager.create(
+                costCenter,
+            )
+
+        return costCenterOrErr
+    }
+}
