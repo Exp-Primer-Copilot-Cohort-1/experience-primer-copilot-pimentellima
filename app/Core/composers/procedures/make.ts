fb4a30ad333dad255fc1817575b71e62ec53c8df
pimentellima <matheus_dere@hotmail.com>
2023-11-22 15:47:15 -0300

    fix: bug não excluir/mostrar excluidos procedimentos

--- app/Core/composers/procedures/make.ts
+++ app/Core/composers/procedures/make.ts
@@ -2,10 +2,11 @@ import { ControllerInjection } from 'App/Core/adapters/controller';
 import { ControllerGeneric } from 'App/Core/adapters/controller/helpers';
 import { CreateProceduresUseCase, UpdateProceduresByIdUseCase } from 'App/Core/domain/use-cases/procedures';
 import { FindAllProceduresByUnityUseCase } from 'App/Core/domain/use-cases/procedures/find-all-procedures-use-case';
+import { IOptsQuery } from 'App/Types/IOptsQuery';
 
 
-export const makeProceduresFindAllComposer = (): ControllerGeneric => {
-	return ControllerInjection.resolve(FindAllProceduresByUnityUseCase)
+export const makeProceduresFindAllComposer = (opts: IOptsQuery): ControllerGeneric => {
+	return ControllerInjection.resolve(FindAllProceduresByUnityUseCase, opts)
 };
 
 export const makeProceduresCreateComposer = (): ControllerGeneric => {
--- app/Core/domain/use-cases/procedures/update-procedures-by-id-use-case.ts
+++ app/Core/domain/use-cases/procedures/update-procedures-by-id-use-case.ts
@@ -14,23 +14,22 @@ export class UpdateProceduresByIdUseCase
 	implements UseCase<Partial<IProcedure>, IProcedure>
 {
 	constructor(
-		@inject(ProceduresMongooseRepository) private readonly manager: ProceduresManagerContract
-	) { }
+		@inject(ProceduresMongooseRepository)
+		private readonly manager: ProceduresManagerContract,
+	) {}
 
-	public async execute(
-		{ _id, ...procedure }: IProcedure,
-	): PromiseEither<AbstractError, IProcedure> {
-		if (!_id) return left(new IdNotProvidedError())
+	public async execute({
+		_id,
+		profs,
+		...procedure
+	}: IProcedure): PromiseEither<AbstractError, IProcedure> {
+		console.log(procedure)
+		try {
+			if (!_id) return left(new IdNotProvidedError())
 
-		const entityOrErr = await ProcedureEntity.build(procedure)
-
-		if (entityOrErr.isLeft()) return entityOrErr
-
-		const entity = entityOrErr.extract()
-
-		return await this.manager.update(
-			_id,
-			entity,
-		)
+			return await this.manager.update(_id, procedure)
+		} catch (err) {
+			return left(new AbstractError('Erro interno', 500))
+		}
 	}
 }
