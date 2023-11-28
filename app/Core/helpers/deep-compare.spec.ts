1eaef5216806c586d80fec64507fe26243417c81
murilomontino <murilomontinojr@hotmail.com>
2023-11-27 22:26:16 -0300

    tests: add deep compare tests for objects with random values

--- app/Core/helpers/deep-compare.spec.ts
+++ app/Core/helpers/deep-compare.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { describe, expect, it } from 'vitest'
 import deepCompare from './deep-compare'
 
@@ -19,9 +20,119 @@ const after = {
 	},
 }
 
-describe('Deep Compare (Unit)', () => {
+const random = ({
+	name = faker.person.firstName(),
+	age = faker.number.int({ min: 18, max: 60 }),
+	address: {
+		street = faker.location.street(),
+		number = faker.number.int({ min: 0, max: 9999 }),
+	},
+	...rest
+}) => ({
+	name,
+	age,
+	address: {
+		street,
+		number,
+	},
+	...rest,
+})
+
+describe('Deep Compare (Only)', () => {
 	it('should compare objects', () => {
-		console.log(deepCompare(before, after))
 		expect(deepCompare(before, after)).toEqual([{ age: 20 }, { age: 21 }])
+	})
+
+	it('should compare objects with random values', () => {
+		const before = random({ address: {} })
+		const after = random({ address: {} })
+		const [original, modified] = deepCompare(before, after)
+		expect(original).toEqual(before)
+		expect(modified).toEqual(after)
+		expect(deepCompare(before, after)).toEqual([before, after])
+	})
+
+	it('should compare objects with name and age', () => {
+		const before = random({
+			name: 'John',
+			age: 20,
+			address: {}
+		})
+		const after = random({
+			name: 'John',
+			age: 20,
+			address: {}
+		})
+		const [original, modified] = deepCompare(before, after)
+		expect(original).toEqual({ address: before.address })
+		expect(modified).toEqual({ address: after.address })
+	})
+
+	it('should compare objects with name and age and address', () => {
+		const before = random({
+			name: 'John',
+			age: 20,
+			address: {
+				street: 'Main Street',
+				number: 123,
+			},
+		})
+		const after = random({
+			name: 'John',
+			age: 20,
+			address: {
+				street: 'Main Street',
+				number: 123,
+			},
+		})
+		const [original, modified] = deepCompare(before, after)
+		expect(original).toEqual({})
+		expect(modified).toEqual({})
+	})
+
+	it('should compare objects with random values plus name and age and address', () => {
+		const before = random({
+			name: 'John',
+			age: 20,
+			address: {
+				street: 'Main Street',
+				number: 123,
+			},
+		})
+		const after = random({
+			name: 'John',
+			age: 20,
+			address: {
+				street: 'Main Street',
+				number: 123,
+			},
+			mother: 'Mary',
+		})
+		const [original, modified] = deepCompare(before, after)
+		expect(original).toEqual({})
+		expect(modified).toEqual({ mother: 'Mary', })
+	})
+
+	it('should compare objects with random values plus name and age and address and mother', () => {
+		const before = random({
+			name: 'John',
+			age: 20,
+			address: {
+				street: 'Main Street',
+				number: 123,
+			},
+			mother: 'Mary',
+		})
+		const after = random({
+			name: 'John',
+			age: 20,
+			address: {
+				street: 'Main Street',
+				number: 123,
+			},
+		})
+		const [original, modified] = deepCompare(before, after)
+		expect(original).toEqual({ mother: 'Mary', })
+		expect(modified).toEqual({})
 	})
 })
